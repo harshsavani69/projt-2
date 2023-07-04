144dfea79fea4c4ba26fec12e3d4bff7bef723e4
harsh savani <131761988+harshsavani69@users.noreply.github.com>
2023-07-04 21:08:07 +0530

    Add files via upload

---
+++ her2-1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+class mother{
+	public :
+		void display()
+		{
+			cout<<"hoe are you?"<<endl;
+		}
+};
+class daughter : public mother
+{
+	public :
+		void display()
+		{
+			cout<<"i am fine"<<endl;
+		}
+};
+	int main()
+	{
+		daughter D;
+		D.mother::display();
+		D.display();
+	}
